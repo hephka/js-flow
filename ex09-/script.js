d6ad31eb91ddbac3df6595a00bd5769be6718766
Paulina Hetman <info@pehaa.com>
2020-07-15 08:38:28 +0200

    Update script.js

--- ex09-/script.js
+++ ex09-/script.js
@@ -1,4 +1,4 @@
 "use strict"
 const start = 2
 const num = 10
-/* affiche les nombres de start, essaye while, do while et for */
+/* affiche les nombres de start au num, essaye while, do while et for */
